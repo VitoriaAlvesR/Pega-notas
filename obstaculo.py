989dccc67563346acf227a36b6e591e0075a3a4f
Vitória Cristina <vitoriaalves.ajax@gmail.com>
2024-05-14 17:07:38 -0300

    Add: Comandos para o obstáculos

--- obstaculo.py
+++ obstaculo.py
@@ -1,1 +1,20 @@
 import pygame
+import random
+
+class Obstaculo:
+
+    def __init__(self,arquivo_imagem,largura_imagem,altura_imagem,x_inicial,y_inicial):
+        self.imagem = pygame.image.load(arquivo_imagem)
+
+        self.largura = largura_imagem
+        self.altura = altura_imagem
+
+        self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
+
+        self.pos_x = x_inicial
+        self.pos_y = y_inicial
+
+        self.velocidade = random.randint(1,20)
+
+        self.mascara = pygame.mask.from_surface(self.imagem)
+
